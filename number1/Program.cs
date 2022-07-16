ab062b57a23bdf2573777d27ebd8437b463d6f72
Pavel Popkov <goil2011@yandex.ru>
2022-07-16 17:49:49 +0300

    number1

---
+++ number1/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+if (n > 9999 && n < 100000)
+{   
+    if ((n/10000 == n % 10) && (n/1000) % 10 == (n/10) % 10)
+    Console.WriteLine("Число является палиндромом " + Convert.ToString(n));
+    else
+    Console.WriteLine("Число не является палиндромом " + Convert.ToString(n));
+}
+else 
+    Console.WriteLine("Введите пятизначное число");
