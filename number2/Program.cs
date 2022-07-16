f7d90a23dffa1cbe2a7a461a25929e0af228eba8
Pavel Popkov <goil2011@yandex.ru>
2022-07-16 19:14:51 +0300

    number2

---
+++ number2/Program.cs
@@ -0,0 +1,10 @@
+﻿int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double distanse = Math.Sqrt(Math.Pow((x1+x2), 3) + Math.Pow((y1+y2), 3) + Math.Pow((z1+z2), 3));
+
+Console.WriteLine("Расстояние между точками: " + distanse);
